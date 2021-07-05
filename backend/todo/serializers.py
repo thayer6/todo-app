82178d3e912cedafa4a262f380b7f0db6031d02f
thayer6 <casey.thayer6@gmail.com>
2021-07-05 12:48:13 -0700

    initial commit

---
+++ backend/todo/serializers.py
@@ -0,0 +1,8 @@
+# need serializers to convert model instances to json so that the frontend can work with the received data
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'description', 'completed')
